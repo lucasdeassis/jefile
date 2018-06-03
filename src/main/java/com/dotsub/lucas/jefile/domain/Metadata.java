6d2ba5125f6014112093b8b9ddab51e08d0e33d6
lucasdeassis <lucasassis413@gmail.com>
2018-06-03 00:02:25 -0300

    add needed getter/setter to metadata id

--- src/main/java/com/dotsub/lucas/jefile/domain/Metadata.java
+++ src/main/java/com/dotsub/lucas/jefile/domain/Metadata.java
@@ -35,6 +35,14 @@ public class Metadata {
     this.contentUrl = contentUrl;
   }
 
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
   public String getName() {
     return name;
   }
